6b78ef107dc64789ade769f85eeeb512a93157a2
Leonel Llane <llane4@hotmail.es>
2022-01-02 01:34:22 -0300

    Stripe

--- src/pages/api/create-payment-intent.js
+++ src/pages/api/create-payment-intent.js
@@ -4,8 +4,6 @@ import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_API_SECRET_KEY);
 
 export default async function createPaymentIntent(req, res) {
-  // const { items } = req.body
-
   const paymentIntent = await stripe.paymentIntents.create({
     amount: 1400,
     currency: "usd",
